1a5b43eea7b056a12a7f4c1d9e52153a2b443b1a
Кирилл Коннов <k-konnov@list.ru>
2022-03-03 20:14:09 +0400

    Add Note Mappings

---
+++ Notes/Shared/DataTransferObjects/CreateNoteDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Notes.Shared.DataTransferObjects;
+
+public class CreateNoteDTO
+{
+	[Display(Name = "Title")]
+	public string? Title { get; set; }
+	
+	[Required]
+	[Display(Name = "Text")]
+	[MinLength(1, ErrorMessage = "The note must contain at least 1 character")]
+	public string? Content { get; set; }
+}
---
+++ Notes/Shared/ViewModels/NoteVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Notes.Shared.ViewModels;
+
+public class NoteVM
+{
+	public Guid Id { get; set; }
+	public string? Title { get; set; }
+	public string? Content { get; set; }
+	public DateTime CreationDate { get; set; }
+}
