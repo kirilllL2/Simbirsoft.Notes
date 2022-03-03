1cdd08ff9f54199989be696d7d0b29f0f09c4d41
Кирилл Коннов <k-konnov@list.ru>
2022-03-03 16:38:23 +0400

    Configure HttpClient

--- Notes/Client/Program.cs
+++ Notes/Client/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Notes.Client;
 
@@ -6,6 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHttpClient("Notes.ServerAPI", client =>
+    client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("Notes.ServerAPI"));
 
 await builder.Build().RunAsync();
