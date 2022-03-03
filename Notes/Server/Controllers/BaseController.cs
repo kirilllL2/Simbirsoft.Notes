eb840c8ae8ffda8ad029041f130054cbd37792b0
Кирилл Коннов <k-konnov@list.ru>
2022-03-03 20:14:26 +0400

    Add Base and Note Controller

---
+++ Notes/Server/Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Notes.Server.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    private IMapper? _mapper;
+
+    protected IMapper Mapper =>
+        _mapper ??= HttpContext.RequestServices.GetService<IMapper>();
+
+    internal Guid UserId => !User.Identity.IsAuthenticated
+        ? Guid.Empty
+        : Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+}
